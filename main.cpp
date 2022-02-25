fbb88ffd33c8d254fd8f48a3f79c52622b18de17
Giuseppe-Bianc <75339264+Giuseppe-Bianc@users.noreply.github.com>
2022-02-25 18:38:01 +0100

    Modifica main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include <SFML/Graphics.hpp>
 #include "Struct.h"
 
@@ -8,27 +10,20 @@ int main(){
 	settings.antialiasingLevel = 8;
 	sf::RenderWindow window(sf::VideoMode(wdta.w, wdta.h), wdta.t, sf::Style::Default, settings);
 
-	sf::CircleShape shape(50.0f, 50);
-	shape.setFillColor(sf::Color::Red);
-	shape.setPosition(sf::Vector2f(100, 100));
-	shape.setOutlineThickness(10);
-	shape.setOutlineColor(sf::Color::Green);
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-	sf::RectangleShape rectangle(sf::Vector2f(200, 20));
-	rectangle.setPosition(sf::Vector2f(300, 100));
-	rectangle.setFillColor(sf::Color::Blue);
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
-				TIME_ITSNL(window.close());
+				TIME_ITSNL(window.close()); 
 				CORE_INFO("chiusura programma in risposta a solleciatazione, tempo chiusura = {} us ({:7.9} ms)",getTime().duration, getTime().ms);
 			}
 		}
 
-		window.clear(sf::Color::Black);
+		window.clear();
 		window.draw(shape);
-		window.draw(rectangle);
 		window.display();
 	}
 	return 0;
